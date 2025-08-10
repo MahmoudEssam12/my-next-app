7a8f4b528eaabc4eb4232635bc088de4c78a5c99
Mahmoud Essam <dev.mahmoud.essam@gmail.com>
2025-08-10 19:33:38 +0300

    update Google Tag Manager ID in _app and _document files

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
-                        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQ5B6443"
+                        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7XHCMKH"
                     height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
       </body>
     </Html>
