5ee178e6296e698e08caea535332fa6a636d37f2
Mahmoud Essam <dev.mahmoud.essam@gmail.com>
2025-08-10 18:14:47 +0300

    added noscript gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+                        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQ5B6443"
+                    height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
       </body>
     </Html>
   );
